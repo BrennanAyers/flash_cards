361b7256a86cb11eb049dbd2fde1d1f9ec5f539f
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-03-14 20:07:03 -0600

    Add string modifcation to Turn class  method to ensure case is a non-issue in answering questions

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,9 +3,9 @@ require './lib/deck'
 require './lib/round'
 
 card_1 = Card.new("What is 5 + 5?", "10", "Math")
-card_2 = Card.new("What is Rachel's favorite animal?", "AARDVARK", "Turing Staff")
-card_3 = Card.new("What is Mike's middle name?", "NOBODY KNOWS", "Turing Staff")
-card_4 = Card.new("What cardboard cutout lives at Turing?", "JUSTIN BIEBER", "Pop Culture")
+card_2 = Card.new("What is Rachel's favorite animal?", "Aardvark", "Turing Staff")
+card_3 = Card.new("What is Mike's middle name?", "nobody knows", "Turing Staff")
+card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", "Pop Culture")
 
 flashcard_deck = Deck.new([card_1, card_2, card_3, card_4])
 
