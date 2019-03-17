92ecbce8c73675fdf9d3c130c2f2fba358d8b10f
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-03-17 14:10:54 -0600

    Add card generation tests and empty.txt file

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -3,15 +3,17 @@ require_relative 'card'
 class CardGenerator
   attr_reader :cards
 
-  def initialize(file_path)
+  def initialize(file_path = "")
     @file_path = file_path
     @cards = []
 
-    File.foreach(@file_path) do |line|
-      question = line.split(",")[0]
-      answer = line.split(",")[1]
-      category = line.rstrip.split(",")[2]
-      @cards << Card.new(question, answer, category)
+    if @file_path != ""
+      File.foreach(@file_path) do |line|
+        question = line.split(",")[0]
+        answer = line.split(",")[1]
+        category = line.rstrip.split(",")[2]
+        @cards << Card.new(question, answer, category)
+      end
     end
   end
 
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -11,7 +11,20 @@ class CardGeneratorTest < Minitest::Test
     assert_instance_of CardGenerator, @generator
   end
 
-  def test_it_has_cards_in_the_deck
+  def test_it_has_cards
+    assert_instance_of Card, @generator.cards[0]
     assert_equal 4, @generator.cards.length
+  end
+
+  def test_it_correctly_generates_the_cards
+    assert_equal "What is 5 + 5?", @generator.cards[0].question
+    assert_equal "What is Rachel's favorite animal?", @generator.cards[1].question
+    assert_equal "What is Mike's middle name?", @generator.cards[2].question
+    assert_equal "What cardboard cutout lives at Turing?", @generator.cards[3].question
+  end
+
+  def test_it_does_not_generate_cards_if_given_empty_file
+    empty_generator = CardGenerator.new('../lib/empty.txt')
+    assert_equal [], empty_generator.cards
   end
 end
