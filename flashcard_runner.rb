854454d52526aea3a36794aaeafeb50770355a0c
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-03-17 14:45:13 -0600

    Modify flashcard_runner to use the card_generator class instead

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,13 +1,18 @@
-require './lib/card'
+# require './lib/card'
 require './lib/deck'
 require './lib/round'
+require './lib/card_generator'
 
-card_1 = Card.new("What is 5 + 5?", 10, "Math")
-card_2 = Card.new("What is Rachel's favorite animal?", "Aardvark", "Turing Staff")
-card_3 = Card.new("What is Mike's middle name?", "nobody knows", "Turing Staff")
-card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", "Pop Culture")
+# card_1 = Card.new("What is 5 + 5?", 10, "Math")
+# card_2 = Card.new("What is Rachel's favorite animal?", "Aardvark", "Turing Staff")
+# card_3 = Card.new("What is Mike's middle name?", "nobody knows", "Turing Staff")
+# card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", "Pop Culture")
+#
+# flashcard_deck = Deck.new([card_1, card_2, card_3, card_4])
 
-flashcard_deck = Deck.new([card_1, card_2, card_3, card_4])
+generator = CardGenerator.new('./lib/cards.txt')
+
+flashcard_deck = Deck.new(generator.cards)
 
 flashcard_round = Round.new(flashcard_deck)
 
