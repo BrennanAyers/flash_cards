d28abb90bea19c8157b766991ea049ec88d67a12
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-03-17 19:45:53 -0600

    Change deck methods from .each to .find_all and .map

--- lib/deck.rb
+++ lib/deck.rb
@@ -10,21 +10,15 @@ class Deck
    end
 
    def cards_in_category(category)
-     matching_cards = []
-     @cards.each do |card|
-      if card.category == category
-        matching_cards << card
-      end
+     @cards.find_all do |card|
+      card.category == category
     end
-    matching_cards
    end
 
-   # def categories_list
-   #   categories_array = []
-   #   @cards.each do |card|
-   #     categories_array << card.category
-   #   end
-   #   categories_array
-   # end
+   def categories_list
+     @cards.map do |card|
+       card.category
+     end.uniq
+   end
 
 end
